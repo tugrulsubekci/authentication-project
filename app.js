410f9137bda4e55f617d433d273c2cf7776ee9b2
Tuğrul Sübekci <104980354+tugrulsubekci@users.noreply.github.com>
2023-04-09 21:36:12 +0300

    login and register sections completed with mongoose-encryption

--- app.js
+++ app.js
@@ -1,1 +1,80 @@
 //jshint esversion:6
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const encryption = require('mongoose-encryption');
+
+const app = express();
+
+app.use(express.static("puclic"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+mongoose.connect("mongodb://127.0.0.1:27017/auth");
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: String
+})
+
+const secret = "Iloveyou";
+userSchema.plugin(encryption, {secret: secret, encryptedFields: ["password"]});
+
+const User = mongoose.model("User", userSchema);
+
+app.get('/', (req, res) => {
+    res.render("home.ejs");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login.ejs");
+});
+
+app.get("/register", (req, res) => {
+    res.render("register.ejs");
+});
+
+app.post("/register", async (req, res) => {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    await newUser.save()
+        .then(function (models) {
+            console.log(models);
+            res.render("secrets.ejs");
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+})
+
+app.post("/login", async (req, res) => {
+    const email = req.body.username;
+    const password = req.body.password;
+
+    await User.findOne({email: email})
+        .then((model) => {
+            console.log(model);
+            if(password === model.password) {
+                res.render("secrets.ejs");
+            }
+            else {
+                console.log("authentication failed");
+            }
+
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+})
+
+app.listen(3000, (req, res) => {
+    console.log("listening on port 3000");
+});
