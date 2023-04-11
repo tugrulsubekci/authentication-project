f62c0937c46672db3f3b69dee28a183b46fdbec8
Tuğrul Sübekci <104980354+tugrulsubekci@users.noreply.github.com>
2023-04-11 23:19:48 +0300

    encrypt password via md5 hashing

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 const encryption = require('mongoose-encryption');
+const md5 = require('md5');
 
 const app = express();
 
@@ -21,8 +22,10 @@ const userSchema = new mongoose.Schema({
     },
     password: String
 })
-const secret = process.env.SECRET;
-userSchema.plugin(encryption, {secret: secret, encryptedFields: ["password"]});
+
+// You can activate below codes for encyript password via mongoose-encryption package
+// const secret = process.env.SECRET;
+// userSchema.plugin(encryption, {secret: secret, encryptedFields: ["password"]});
 
 const User = mongoose.model("User", userSchema);
 
@@ -41,7 +44,7 @@ app.get("/register", (req, res) => {
 app.post("/register", async (req, res) => {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     await newUser.save()
@@ -56,7 +59,7 @@ app.post("/register", async (req, res) => {
 
 app.post("/login", async (req, res) => {
     const email = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     await User.findOne({email: email})
         .then((model) => {
